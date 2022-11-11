49499768edafe34b8387c2605d2842ddad890bc0
Sergio <winksousa0@gmail.com>
2022-11-11 11:35:12 -0300

    vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 import { NavBar } from "../components/NavBar";
 import "../styles/globals.css";
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider attribute="class">
       <NavBar />
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
